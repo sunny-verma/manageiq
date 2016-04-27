79d7e89f533b80915cb99811e70a5f3f6810a628
samuel.choppara@alcatel-lucent.com <samuel.choppara@alcatel-lucent.com>
2016-04-27 15:15:04 -0400

    Network Maanger vsd login

--- app/models/manageiq/providers/nuage/network_manager.rb
+++ app/models/manageiq/providers/nuage/network_manager.rb
@@ -1,6 +1,30 @@
-Class ManageIQ::Providers::Nuage::NetworkManager < ManageIQ::Providers::NetworkManager
+class ManageIQ::Providers::Nuage::NetworkManager < ManageIQ::Providers::NetworkManager
   require_nested :RefreshParser
   require_nested :RefreshWorker
   require_nested :Refresher
+  require_nested :VsdClient
+
+  def self.raw_connect(auth_url, username, password)
+
+    VsdClient.new(auth_url, username, password)
+
+  end
+
+  def connect(options = {})
+    raise "no credentials defined" if self.missing_credentials?(options[:auth_type])
+
+    protocol = options[:protocol]
+    server   = options[:ip] || address
+    port     = options[:port] || self.port
+    username = options[:user] || authentication_userid(options[:auth_type])
+    password = options[:pass] || authentication_password(options[:auth_type])
+    version  = options[:version] || api_version
+
+    self.class.raw_connect(auth_url(protocol, server, port ,version), username, password)
+  end
+
+  def auth_url(protocol, server, port, version)
+    return protocol+'://' + server + ':' + port + '/' + 'nuage/api/' + version
+  end
 
 end
