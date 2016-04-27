0646918e8f5906904cb832bbc3bfbc5e77d9a193
sunny-verma <sunnyverma1992@gmail.com>
2016-04-27 13:03:43 -0400

    Created network_manager.rb file

---
+++ app/models/manageiq/providers/nuage/network_manager.rb
@@ -0,0 +1,6 @@
+Class ManageIQ::Providers::Nuage::NetworkManager < ManageIQ::Providers::NetworkManager
+  require_nested :RefreshParser
+  require_nested :RefreshWorker
+  require_nested :Refresher
+
+end
