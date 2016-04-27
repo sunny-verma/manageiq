d71612eabac9148cf96a43c76b454dd6123cebe1
Rohit Pagedar <rohit.pagedar@alcatel-lucent.com>
2016-04-27 16:53:00 -0400

    Partial Commit before fetch, refresh_parser

--- app/models/manageiq/providers/nuage/network_manager/refresher.rb
+++ app/models/manageiq/providers/nuage/network_manager/refresher.rb
@@ -1,2 +1,13 @@
-class ManageIQ::Providers::Nuage::NetworkManager::Refresher < ManageIQ::Providers::BaseManager::Refresher
+module ManageIQ::Providers
+  class Nuage::NetworkManager::Refresher < ManageIQ::Providers::BaseManager::Refresher
+    include ::EmsRefresh::Refreshers::EmsRefresherMixin
+
+    def parse_legacy_inventory(ems)
+      ManageIQ::Providers::Nuage::NetworkManager::RefreshParser.ems_inv_to_hashes(ems, refresher_options)
+    end
+
+    def post_process_refresh_classes
+      []
+    end
+  end
 end
