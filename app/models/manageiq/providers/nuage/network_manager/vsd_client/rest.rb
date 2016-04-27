9b7875d992d47882dc3f759a2b4e6d76bb967a84
sunny-verma <sunnyverma1992@gmail.com>
2016-04-27 18:18:27 -0400

    Added VSD Client

---
+++ app/models/manageiq/providers/nuage/network_manager/vsd_client/rest.rb
@@ -0,0 +1,76 @@
+require 'rest-client'
+require 'rubygems'
+require 'json'
+  class ManageIQ::Providers::Nuage::NetworkManager::Rest
+
+    def initialize(server, user, password)
+      @server=server
+      @user=user
+      @password=password
+      @apiKey=''
+      @headers = {'X-Nuage-Organization' => 'csp', "Content-Type" => "application/json; charset=UTF-8"}
+    end
+
+    def login
+      @loginUrl = @server+"/me"
+      RestClient::Request.execute(method: :get, url: @loginUrl, user: @user, password: @password, headers: @headers) { |response|
+        case response.code
+          when 200
+            data = JSON.parse(response.body)
+            data1 = data[0]
+            @apiKey = data1["APIKey"]
+            return true, data1["enterpriseID"]
+          else
+            return false, nil
+        end
+      }
+
+    end
+
+    def server
+      return @server
+    end
+
+    def appendHeaders(key, value)
+      @headers[key]=value
+    end
+
+    def get(url)
+      if (@apiKey == '')
+        login
+      end
+      RestClient::Request.execute(method: :get, url: url, user: @user, password: @apiKey, headers: @headers) { |response|
+        return response
+      }
+    end
+
+    def delete(url)
+      if (@apiKey == '')
+        login
+      end
+      RestClient::Request.execute(method: :delete, url: url, user: @user, password: @apiKey, headers: @headers) { |response|
+        return response
+      }
+    end
+
+    def put(url, data)
+      if (@apiKey == '')
+        login
+      end
+
+      RestClient::Request.execute(method: :put, data: data, url: url, user: @user, password: @apiKey, headers: @headers) { |response|
+        return response
+      }
+    end
+
+    def post(url, data)
+      if (@apiKey == '')
+        login
+      end
+
+      RestClient::Request.execute(method: :post, data: data, url: url, user: @user, password: @apiKey, headers: @headers) { |response|
+        return response
+      }
+    end
+
+  end
