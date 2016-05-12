0ca6849510909c313c66b3419faffa7f5e4782d9
Ladislav Smola <lsmola@redhat.com>
2016-05-12 12:46:36 +0200

    Add default NetworkGroup, so CloudSubnets save correctly
    Add default NetworkGroup, so CloudSubnets save correctly

--- app/models/manageiq/providers/nuage/network_manager/refresh_parser.rb
+++ app/models/manageiq/providers/nuage/network_manager/refresh_parser.rb
@@ -30,15 +30,15 @@ module ManageIQ::Providers
 
     def get_enterprises
       enterprises = @vsd_client.get_enterprises
-      enterprises.each { |enterprise| 
+      enterprises.each { |enterprise|
         @enterprises[enterprise['ID']] = enterprise['name']
      }
      get_domains
     end
-    
+
     def get_domains
       domains = @vsd_client.get_domains
-      domains.each { |domain| 
+      domains.each { |domain|
         @domains[domain['ID']] = domain['name'], domain['parentID'], @enterprises[domain['parentID']]
      }
       get_zones
@@ -46,7 +46,7 @@ module ManageIQ::Providers
 
     def get_zones
       zones = @vsd_client.get_zones
-      zones.each { |zone| 
+      zones.each { |zone|
         domain = @domains[zone['parentID']]
         @zones[zone['ID']] = zone['name'], zone['parentID'], domain[0], domain[1], domain[2]
      }
@@ -54,18 +54,42 @@ module ManageIQ::Providers
     end
 
     def get_subnets
-      subnets = @vsd_client.get_subnets
-      process_collection(subnets, :cloud_subnets) { |s| parse_subnets(s) }
+      # TODO for now creating just default NetworkGroup, later we should fetch a correct groups of subnets
+      process_collection([{:uid => 'default'}], :network_groups) { |n| parse_network_group(n) }
+
+      @data[:cloud_subnets] = []
+      @data[:network_groups].each do |net|
+        net[:cloud_subnets] = @vsd_client.get_subnets.collect { |s| parse_subnet(s) }
+
+        # Lets store also subnets into indexed data, so we can reference them elsewhere
+        net[:cloud_subnets].each do |x|
+          @data_index.store_path(:cloud_subnets, x[:ems_ref], x)
+          @data[:cloud_subnets] << x
+        end
+      end
     end
 
     def to_cidr(netmask)
       '/' + netmask.to_i.to_s(2).count("1").to_s
     end
 
-    def parse_subnets(subnet)
-      uid = subnet['ID']
+    def parse_network_group(network_group)
+      uid     = network_group[:uid]
+      status  = "active"
 
       new_result = {
+        :type                      => self.class.network_group_type,
+        :name                      => uid,
+        :ems_ref                   => uid,
+        :status                    => status,
+      }
+      return uid, new_result
+    end
+
+    def parse_subnet(subnet)
+      uid = subnet['ID']
+
+      {
         :type             => self.class.cloud_subnet_type,
         :name             => subnet['name'],
         :ems_ref          => uid,
@@ -76,9 +100,8 @@ module ManageIQ::Providers
         :ip_version       => 4,
         :extra_attributes => map_extra_attributes(subnet['parentID'])
       }
-      return uid, new_result
     end
-    
+
     def map_extra_attributes(subnet_parent_id)
       zone_id              = subnet_parent_id
       zone                 = @zones[subnet_parent_id]
@@ -87,13 +110,17 @@ module ManageIQ::Providers
       domain_name          = zone[2]
       enterprise_id        = zone[3]
       enterprise_name      = zone[4]
-      return {'enterprise_name' => enterprise_name, 'enterprise_id' => enterprise_id, 
+      return {'enterprise_name' => enterprise_name, 'enterprise_id' => enterprise_id,
         'domain_name' => domain_name, 'domain_id' => domain_id, 'zone_name' => zone_name, 'zone_id' => zone_id}
     end
-   
+
     class << self
       def cloud_subnet_type
         "ManageIQ::Providers::Nuage::NetworkManager::CloudSubnet"
+      end
+
+      def network_group_type
+        "ManageIQ::Providers::Nuage::NetworkManager::NetworkGroup"
       end
     end
   end
