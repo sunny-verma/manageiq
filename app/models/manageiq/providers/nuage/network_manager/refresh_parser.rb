2167a245ff7f85df1ac7568d04f1a5a2161a8646
samuel-nuage <samuel.choppara@nuagenetworks.net>
2016-06-22 15:31:30 -0400

    Local setup hardcode

--- app/models/manageiq/providers/nuage/network_manager/refresh_parser.rb
+++ app/models/manageiq/providers/nuage/network_manager/refresh_parser.rb
@@ -141,7 +141,7 @@ module ManageIQ::Providers
         :type                => self.class.security_group_type,
         :ems_ref             => uid,
         :name                => pg['name'],
-        :network_group    => NetworkGroup.where(:name => "Raleigh-NOC").first  
+        :network_group    => NetworkGroup.where(:name => "Nuage_Partition").first  
       }
 
       return uid, new_result
